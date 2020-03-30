d745eaa6b13464ccc415f310f9d1d0286f167279
andrewsimpsonphl <andrew.simpson@phila.gov>
2020-03-30 13:21:11 -0400

    Adding infodev and uta data to ./data
    Updating segmentation_code with functions for each step

--- segmentation_code.R
+++ segmentation_code.R
@@ -1,8 +1,9 @@
 # external scrips for usage.rmd
 
 library(tidyverse) ; library(httr) ; library(jsonlite) ; library(geojsonsf)
-library(sf)  ;  library(leaflet)  ;  library(htmltools) 
+library(sf)  ;  library(leaflet)  ;  library(htmltools) ; library(knitr)
 
+## @knitr pull_data
 pull_arcgis_dat <- function() {
   path_geo_0_4000 <- "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/segmentation_v1_shp/FeatureServer/0/query?where=FID+%3E+0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=standard&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=HodualTVpMTIigvp2LNpZBFGKGm8GECP4vbiJIkTdhREtbkW1jYXBrAqiLAq5BSZ2Kq9ZW7zWWZtwaYkZMh4jakgBiP9DAWidMf6NP4BTvb4tHyeQq5uSL7ICCKQufS8U8Oq4f7irzgmWLAjTs_dLGaQ-ZNQV1ZB5TVLrSNhNltztF1FXxJ7rKUdmjQFjZa4jwDoDtb8AZoanNj5EGajYYdYLGTnulcw_erSXZVPXd2Z0fcU77Ha56p4RGDARLjP"
   path_geo_4001_8000 <- "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/segmentation_v1_shp/FeatureServer/0/query?where=FID+%3E+0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=standard&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=4001&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=HodualTVpMTIigvp2LNpZBFGKGm8GECP4vbiJIkTdhREtbkW1jYXBrAqiLAq5BSZ2Kq9ZW7zWWZtwaYkZMh4jakgBiP9DAWidMf6NP4BTvb4tHyeQq5uSL7ICCKQufS8U8Oq4f7irzgmWLAjTs_dLGaQ-ZNQV1ZB5TVLrSNhNltztF1FXxJ7rKUdmjQFjZa4jwDoDtb8AZoanNj5EGajYYdYLGTnulcw_erSXZVPXd2Z0fcU77Ha56p4RGDARLjP"
@@ -39,3 +40,29 @@ read_links <- function() {
   
   return()
 }
+
+
+## @knitr load_data
+
+load_segments <- function(segmentation_dat) {
+  segments_loaded <- segmentation_dat %>% 
+    left_join(stops %>% data.frame() %>% select(-geometry), by = c("fromto" = "combo")) %>%
+    filter(BASE_ID > 0 & BASE_ID < 900) %>%
+    filter(TYPE > 0 & TRANSIT > 0 & OTHER > 0) %>%
+    group_by(FINAL_ID) %>%
+    summarise(load = mean(total_load, na.rm = TRUE), stops = list(unique(stop_id)))
+  
+  return(segments_loaded)
+}
+
+segment_geometry <- function(segmentation_dat) {
+  segments_geometry <- segmentation_dat %>% 
+    filter(BASE_ID > 0 & BASE_ID < 900) %>%
+    filter(TYPE > 0 & TRANSIT > 0 & OTHER > 0) %>%
+    group_by(FINAL_ID) %>%
+    summarise(length = sum(Shape__Length))
+  
+  return(segments_geometry)
+}
+
+
