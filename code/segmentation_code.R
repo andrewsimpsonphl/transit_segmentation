1a910f648a62cd29bc5f7106d0fd5ac4c93b53d2
andrewsimpsonphl <andrew.simpson@phila.gov>
2020-04-01 12:32:54 -0400

    Updating usage.rmd to walk user through code.

--- code/segmentation_code.R
+++ code/segmentation_code.R
@@ -69,7 +69,7 @@ nest_segments <- function(coded_links, stops) {
 
 #nested_segments <- nest_segments(coded_links, stops)
 
-## @knitr apc_dat functions
+## @knitr compile_dat functions
 
 #import apc trip data
 import_apc <- function() {
@@ -214,21 +214,21 @@ add_analytics <- function(compiled_apc_dat, gis_dat) {
 ## @knitr final_run
 
 # step 1: create your stop file
-stops <- read_stops()
+#stops <- read_stops()
 
 # step 2: pull in coded links from AGO 
-gis_dat <- pull_arcgis_dat() 
+#gis_dat <- pull_arcgis_dat() 
 
-nested_data <- gis_dat %>%
-  load_coded_links(stops) %>%  # step 3.1: load coded links with stop_ids
-  nest_segments(stops)         # step 3.2: nest the links into segments with FINAL_IDs
+#nested_data <- gis_dat %>%
+  #load_coded_links(stops) %>%  # step 3.1: load coded links with stop_ids
+  #nest_segments(stops)         # step 3.2: nest the links into segments with FINAL_IDs
   
-FINAL_ID_LIST <- unique((as.numeric(nested_data$FINAL_ID)))
+#FINAL_ID_LIST <- unique((as.numeric(nested_data$FINAL_ID)))
 
 # step 3.3: compile APC trip data to the segment level
-segments_with_apc_dat <- nested_data %>% 
-  filter(between(as.numeric(FINAL_ID), FINAL_ID_LIST[1], FINAL_ID_LIST[100])) %>%
-  compile_apc_dat()  
+#segments_with_apc_dat <- nested_data %>% 
+  #filter(between(as.numeric(FINAL_ID), FINAL_ID_LIST[1], FINAL_ID_LIST[100])) %>%
+  #compile_apc_dat()  
 
 # step 4: run analytics on each segment
-segments_with_apc_analytics <- segments_with_apc_dat %>% add_analytics(gis_dat) 
+#segments_with_apc_analytics <- segments_with_apc_dat %>% add_analytics(gis_dat) 
